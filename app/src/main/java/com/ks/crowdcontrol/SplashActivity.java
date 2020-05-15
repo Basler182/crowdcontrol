2c25c1c019eeab025b535a9033ea487aea92d474
Basler182 <showdialog@web.de>
2020-05-15 23:00:24 +0200

    feature: added firebase sdk

--- app/src/main/java/com/ks/crowdcontrol/SplashActivity.java
+++ app/src/main/java/com/ks/crowdcontrol/SplashActivity.java
@@ -2,12 +2,16 @@ package com.ks.crowdcontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -25,13 +29,19 @@ public class SplashActivity extends AppCompatActivity {
         //Animations
         topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
         bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
-        //
-       image = findViewById(R.id.splash_logo_img);
+        //Initializing
+        image = findViewById(R.id.splash_logo_img);
         logo = findViewById(R.id.splash_title);
         slogan = findViewById(R.id.splash_desc);
         //Set animation to elements
         image.setAnimation(topAnim);
         logo.setAnimation(bottomAnim);
         slogan.setAnimation(bottomAnim);
+        //Starts Main Activity
+        new Timer().schedule(new TimerTask() {
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }, 2000 /*amount of time in milliseconds before execution*/);
     }
 }
